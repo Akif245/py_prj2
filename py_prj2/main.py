eec580d76cdea584af9b8cb0f3ea7d8a99da909a
Abrar944 <ahmedabrar944@gmail.com>
2024-02-27 23:44:22 +0530

    Login form with python

---
+++ py_prj2/main.py
@@ -0,0 +1,11 @@
+import tkinter
+
+
+main=tkinter.Tk()
+#TK helps to display roots on window and manage all code / Application
+main.title("Basic form ")
+main.geometry('340x440')
+# main.configure(bg='#333333')
+
+main.mainloop()
+# no code can be executed after main loop
